82e3e401563fcd3fd7bd728da4a0d73a5f20eddb
partg952 <dankparth@gmail.com>
2022-04-26 13:25:47 +0530

    h

--- roms-scraper.js
+++ roms-scraper.js
@@ -7,12 +7,13 @@ const app = express.Router();
 
 app.get("/api/search",(req,res)=>{
     let keyword = req.query['q'];
-    let dataarr = []
+    const dataarr = []
     let page_number = req.query['page']
     request(`https://www.gamulator.com/search.php?currentpage=${page_number}&search_term_string=${keyword}`).on('data',(data)=>{
        const $ = Cheerio.load(data);
        $('img.img-fluid').each(function(i){
            dataarr.push({
+
               img: 'https://www.gamulator.com'+$(this).attr('src')  ,
               title:$('h5.card-title').eq(i).text(),
               url:$('div.card > a').eq(i).attr('href'),
@@ -36,21 +37,27 @@ app.get("/api/download",(req,res)=>{
 
 app.get("/api/details",(req,res)=>{
     let url = req.query.url;
-    let dataarr = [];
+    let dataarr = {};
+    const details = [];
     request("https://www.gamulator.com"+url).on('data',data=>{
         const $ = Cheerio.load(data);
-        dataarr.push($('img.img-fluid').attr("src"))
-        dataarr.push($('div.naslov > h1').text())
-        $('table.table > tbody > tr').each(function(i){
-            dataarr.push($(this).text())
+        $("table.table > tbody > tr").each(function(i){
+          details.push($("table.table > tbody > tr").eq(i).text())
         })
+        dataarr = {
+          title:$('div.naslov > h1').text(),
+          image:$("img.img-fluid").attr("src"),
+          desc:$("div.desc > p").text(),
+          details:details
+        }
+        res.send(dataarr);
     })
-    res.send(dataarr);
+
 })
 
 app.get("/api/homepage/:page",(req,res)=>{
     let page = req.params.page;
-    let dataarr = [];
+    const dataarr = [];
     request('https://www.gamulator.com/roms/psp?currentpage='+page).on('data',data=>{
         const $ = Cheerio.load(data);
         $('img.img-fluid').each(function(i){
@@ -65,6 +72,20 @@ app.get("/api/homepage/:page",(req,res)=>{
         res.send(dataarr);
 
     })
+})
+
+app.get("/api/consoles",(req,res)=>{
+  request("https://gamulator.com/roms").on("data",data=>{
+    const $ = Cheerio.load(data);
+    const dataarr = [];
+    $("div.thumbnail-home").each(function(i){
+      dataarr.push({
+        image:$("div.thumbnail-home > a > img").eq(i).attr("src"),
+        name:$("div.thumbnail-home > h3").eq(i).text()
+      })
+    })
+    res.send(dataarr);
+  })
 })
 const romsScraper = app;
 module.exports = romsScraper;
